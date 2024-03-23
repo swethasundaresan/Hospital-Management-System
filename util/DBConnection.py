84bb550faf8ef96580a1d37573e18ecb7f79a67e
SWETHA S <90144135+swethasundaresan@users.noreply.github.com>
2024-03-23 12:36:09 +0530

    Update DBConnection.py

--- util/DBConnection.py
+++ util/DBConnection.py
@@ -12,7 +12,6 @@ class DBConnection:
             if DBConnection.connection is None:
                 connection_string = util.PropertyUtil.getPropertyString()
                 DBConnection.connection = pyodbc.connect(connection_string)
-                print("Database connection successful")
             return DBConnection.connection
         except pyodbc.Error as e:
             print(f"Error connecting to database: {e}")
